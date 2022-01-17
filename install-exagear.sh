9b01a20a980fe2ddf4849654b246b8d0bcf0d071
ShadowNightX <68839412+ShadowNightX@users.noreply.github.com>
2022-01-17 11:57:37 +1100

    Update install-exagear.sh

--- install-exagear.sh
+++ install-exagear.sh
@@ -2,6 +2,8 @@ sudo apt-get update
 sudo apt-get install -y bash coreutils findutils curl binfmt-support cron
 echo "Downloading Exagear"
 
+
+# Comment these lines if you are running 64-bit.
 wget https://archive.org/download/exagear-desktop_202111/exagear_3428-1_armhf.deb
 wget https://archive.org/download/exagear-desktop_202111/exagear-dsound-server_010_armhf.deb
 wget https://archive.org/download/exagear-desktop_202111/exagear-guest-debian-9_3428_all.deb
@@ -9,7 +11,9 @@ echo "Installing Exagear"
 sudo dpkg -i exagear_3428-1_armhf.deb
 sudo dpkg -i exagear-dsound-server_010_armhf.deb
 sudo dpkg -i exagear-guest-debian-9_3428_all.deb
-# Uncomment these lines if you ar running 64-bit and comment the above 7 ones.
+
+
+# Uncomment these lines if you ar running 64-bit.
 #wget https://archive.org/download/exagear-desktop_202111/exagear_3428-1_arm64.deb
 #wget https://archive.org/download/exagear-desktop_202111/exagear-dsound-server_010_arm64.deb
 #wget https://archive.org/download/exagear-desktop_202111/exagear-guest-debian-9_3428_all.deb
@@ -18,7 +22,9 @@ sudo dpkg -i exagear-guest-debian-9_3428_all.deb
 #sudo dpkg -i exagear-dsound-server_010_arm64.deb
 #sudo dpkg -i exagear-guest-debian-9_3428_all.deb
 
+
 echo "Applying Patch"
 curl https://archive.org/download/exagear-desktop_202111/patch.sh | sudo bash
 echo "Finishing Up..."
 sudo apt autoremove -y
+echo "Exagear is now installed! Type the command 'exagear' in terminal to start up your x86 sub-system!"
