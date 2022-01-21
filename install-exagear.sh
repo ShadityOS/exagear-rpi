117114928b2a5e5562cb8459f1257c9b9e4707f4
Crilum <91354257+Crilum@users.noreply.github.com>
2022-01-20 23:35:59 -0700

    Update install-exagear.sh

--- install-exagear.sh
+++ install-exagear.sh
@@ -1,30 +1,28 @@
-sudo apt-get update
-sudo apt-get install -y bash coreutils findutils curl binfmt-support cron
-echo "Downloading Exagear"
+#!/bin/bash
+sudo apt-get update && sudo apt-get install -y bash coreutils findutils curl binfmt-support cron dpkg
 
+echo "Determining arch..."
 
-# Comment these lines if you are running 64-bit.
-wget https://archive.org/download/exagear-desktop_202111/exagear_3428-1_armhf.deb
-wget https://archive.org/download/exagear-desktop_202111/exagear-dsound-server_010_armhf.deb
+if [ $(dpkg --print-architecture) == "armhf" ]; then
+arch=armhf
+elif [ $(dpkg --print-architecture) != "armhf" ] && [ $(uname -m) == "arm64" ] || [ $(uname -m) == "aarch64" ]; then
+arch=arm64
+else
+echo "Unable to determine arch!"
+fi
+echo "Your arch is: $arch"
+echo
+
+echo "Downloading Exagear..."
+
+wget https://archive.org/download/exagear-desktop_202111/exagear_3428-1_$arch.deb
+wget https://archive.org/download/exagear-desktop_202111/exagear-dsound-server_010_$arch.deb
 wget https://archive.org/download/exagear-desktop_202111/exagear-guest-debian-9_3428_all.deb
+
 echo "Installing Exagear"
-sudo dpkg -i exagear_3428-1_armhf.deb
-sudo dpkg -i exagear-dsound-server_010_armhf.deb
-sudo dpkg -i exagear-guest-debian-9_3428_all.deb
 
+sudo apt install ./exagear_3428-1_$arch.deb ./exagear-dsound-server_010_$arch.deb ./exagear-guest-debian-9_3428_all.deb
 
-# Uncomment these lines if you ar running 64-bit.
-#wget https://archive.org/download/exagear-desktop_202111/exagear_3428-1_arm64.deb
-#wget https://archive.org/download/exagear-desktop_202111/exagear-dsound-server_010_arm64.deb
-#wget https://archive.org/download/exagear-desktop_202111/exagear-guest-debian-9_3428_all.deb
-#echo "Installing Exagear"
-#sudo dpkg -i exagear_3428-1_arm64.deb
-#sudo dpkg -i exagear-dsound-server_010_arm64.deb
-#sudo dpkg -i exagear-guest-debian-9_3428_all.deb
-
-
-echo "Applying Patch"
+echo "Applying Patch..."
 curl https://archive.org/download/exagear-desktop_202111/patch.sh | sudo bash
-echo "Finishing Up..."
-sudo apt autoremove -y
 echo "Exagear is now installed! Type the command 'exagear' in terminal to start up your x86 sub-system!"
